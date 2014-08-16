e6b91601f1670f155e21e88a2a8ece9fadc11226
Ryan S. Brown <sb@ryansb.com>
2014-08-16 15:22:34 -0400

    correct app path for openshift
    Former-commit-id: fda151f0644591755375da30cbfc88c4e4c061c4 [formerly b551be4ae106aa4bc2c0c9651a89e2f3912e704f] [formerly 742ed4fa6151a8021e7ac75144ca2f5434d59c3b]
    Former-commit-id: 0cd6f41fc92ef286ac353b460fe6a08370a7dc81 [formerly f82536c85cfd6c607337e7086bf5fe507de7999f]
    Former-commit-id: eb4255606a39145df05b8c40afc6626885f95334

---
+++ hflossk/cli/__init__.py
@@ -0,0 +1,78 @@
+"""
+Author: Ryan Brown <sb@ryansb.com>
+License: Apache 2.0
+"""
+
+import os
+
+import click
+import datetime
+from distutils import dir_util
+from distutils import file_util
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def run():
+    from hflossk.site import app
+    app.run(
+        debug=True,
+        threaded=True,
+    )
+
+
+@cli.command()
+def new():
+    # \u2714 is a check mark
+    # \u2717 is an x
+    # TODO: include default README with instructions for starting your course
+    print(u'\u2714 Glorious README')
+
+    source_dir = os.path.split(__file__)[0].replace('cli', '')
+
+    static_dir = os.path.join(os.getcwd(), 'static')
+    dir_util.copy_tree(source_dir + 'static', static_dir, update=True)
+
+    print(u'\u2714 CSS/Javascript for browser art')
+
+    templates_dir = os.path.join(os.getcwd(), 'templates')
+    dir_util.copy_tree(source_dir + 'templates', templates_dir, update=True)
+
+    print(u'\u2714 Starter Mako templates for great good')
+
+    yamls_dir = os.path.join(source_dir, 'yamls')
+
+    people_dir = os.path.join(os.getcwd(), 'people', year(), season())
+    if not os.path.isdir(people_dir):
+        os.makedirs(people_dir)
+
+    file_util.copy_file(os.path.join(yamls_dir, 'fake_student.yaml'),
+                        people_dir, update=True)
+
+    file_util.copy_file(os.path.join(yamls_dir, 'site.yaml'),
+                        os.getcwd(), update=True)
+    file_util.copy_file(os.path.join(yamls_dir, 'schedule.yaml'),
+                        os.getcwd(), update=True)
+
+    print(u'\u2714 Starter yaml files for data driven education')
+
+
+@cli.command()
+def version():
+    print("You are using hflossk version 0.5.2")
+    print("Get more information at "
+          "https://github.com/decause/hflossk")
+
+
+def season():
+    if datetime.date.today().month > 6:
+        return "fall"
+    return "spring"
+
+
+def year():
+    return str(datetime.date.today().year)

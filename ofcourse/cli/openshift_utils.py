48787b5e6910c4e2cd4f1dfc424ea48e558e4317
Ryan S. Brown <sb@ryansb.com>
2015-02-04 17:08:17 -0500

    resolve #10 by inserting the current ofcourse version into setup_requires

--- ofcourse/cli/openshift_utils.py
+++ ofcourse/cli/openshift_utils.py
@@ -5,6 +5,7 @@ License: Apache 2.0
 
 import logging
 import os
+import pkg_resources
 import re
 import six
 import socket
@@ -30,8 +31,8 @@ setup(name='thecourse',
       author='Dr. Professor',
       author_email='dr@professor.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['ofcourse'],
-     )""",
+      install_requires=['ofcourse>={version}'],
+     )""".format(version=pkg_resources.get_distribution('ofcourse').version),
     },
     "wsgi.py": {
         "contents": """#!/usr/bin/python
@@ -52,61 +53,6 @@ import ofcourse.site
 application = ofcourse.site.app""",
     },
 }
-
-openshift_patch = """
-From f5d949dfb8942c3c2e1996d1209816ed371f20a8 Mon Sep 17 00:00:00 2001
-From: "Ryan S. Brown" <sb@ryansb.com>
-Date: Sat, 16 Aug 2014 16:04:58 -0400
-Subject: [PATCH] add setup and wsgi files
-
----
- setup.py | 10 ++++++++++
- wsgi.py  | 16 ++++++++++++++++
- 2 files changed, 26 insertions(+)
- create mode 100644 setup.py
- create mode 100644 wsgi.py
-
-diff --git a/setup.py b/setup.py
-new file mode 100644
-index 0000000..0b3a53c
---- /dev/null
-+++ b/setup.py
-@@ -0,0 +1,10 @@
-+from setuptools import setup
-+
-+setup(name='thecourse',
-+      version='1.0',
-+      description='courseware on openshift',
-+      author='Dr. Professor',
-+      author_email='dr@professor.com',
-+      url='http://www.python.org/sigs/distutils-sig/',
-+      install_requires=['ofcourse>=0.5.3'],
-+     )
-diff --git a/wsgi.py b/wsgi.py
-new file mode 100644
-index 0000000..409ddfb
---- /dev/null
-+++ b/wsgi.py
-@@ -0,0 +1,16 @@
-+#!/usr/bin/python
-+# IMPORTANT: Please do not make changes to this file unless you know what
-+# you're doing. Thank you.
-+
-+import os
-+
-+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-+try:
-+    execfile(virtualenv, dict(__file__=virtualenv))
-+except IOError:
-+    pass
-+
-+import ofcourse.site
-+
-+application = ofcourse.site.app
---
-1.9.3
-"""
 
 
 class TempBranch(object):

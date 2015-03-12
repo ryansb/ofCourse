d04188c88728694a7ade4c1a6f72cb9a0ca4aeef
Ryan S. Brown <sb@ryansb.com>
2015-03-12 09:31:23 -0400

    Add runtime dep on validator.py and fix .travis.yml copy directive

--- ofcourse/cli/__init__.py
+++ ofcourse/cli/__init__.py
@@ -73,8 +73,8 @@ def new():
                         os.getcwd(), update=True)
     file_util.copy_file(os.path.join(yamls_dir, 'oer.yaml'),
                         os.getcwd(), update=True)
-
-    file_util.copy_file(os.path.join(".travis.yml"), os.getcwd(), update=True)
+    file_util.copy_file(os.path.join(source_dir, ".travis.yml"),
+                        os.getcwd(), update=True)
 
     click.echo(u'\u2714 Starter yaml files for data driven education')
 

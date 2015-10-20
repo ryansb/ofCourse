f2e5d284e0677041c7ee9c24ea0e5e0c03a78bdb
Ryan S. Brown <sb@ryansb.com>
2015-10-20 09:30:17 -0400

    Test `ofcourse new` runs successfully in a tmpdir

--- ofcourse/tests/__main__.py
+++ ofcourse/tests/__main__.py
@@ -1,3 +1,5 @@
 import test_yaml
+import test_new
 
 test_yaml.run_tests()
+test_new.run_tests()
---
+++ ofcourse/tests/test_new.py
@@ -0,0 +1,10 @@
+import subprocess
+import shutil
+import tempfile
+import unittest
+
+class TestOfcourseNew(unittest.TestCase):
+    def test_new(self):
+        dir = tempfile.mkdtemp('ofcoursetest')
+        subprocess.check_call(['ofcourse', 'new'], cwd=dir)
+        shutil.rmtree(dir)
